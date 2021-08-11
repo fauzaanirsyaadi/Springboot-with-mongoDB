af278ce612cf34a0dd50a7e804b01220491137f7
fauzaanirsyaadi <fauzaan.irsyaadi@gmail.com>
2021-08-11 14:33:47 +0700

    [CREATE] add env

---
+++ src/test/java/com/fauzaan/springbootmongodb/SpringbootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fauzaan.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
