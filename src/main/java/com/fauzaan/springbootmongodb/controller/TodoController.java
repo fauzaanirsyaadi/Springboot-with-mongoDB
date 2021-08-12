67ca471686b4c4a3ec7b613b063eb4ad3837bb1d
fauzaanirsyaadi <fauzaan.irsyaadi@gmail.com>
2021-08-12 14:18:36 +0700

    [CREATE]add end point

---
+++ src/main/java/com/fauzaan/springbootmongodb/controller/TodoController.java
@@ -0,0 +1,27 @@
+package com.fauzaan.springbootmongodb.controller;
+
+import com.fauzaan.springbootmongodb.model.TodoDTO;
+import com.fauzaan.springbootmongodb.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TodoController {
+    @Autowired
+    private TodoRepository todoRepo;
+    @GetMapping("/todos")
+    public ResponseEntity<?> getAllTodos(){
+        List<TodoDTO> todos = todoRepo.findAll();
+        if(todos.size()>0){
+            return new ResponseEntity<List<TodoDTO>>(todos, HttpStatus.OK);
+        }else{
+            return new ResponseEntity("No todos available", HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
--- src/main/java/com/fauzaan/springbootmongodb/repository/TodoRepository.java
+++ src/main/java/com/fauzaan/springbootmongodb/repository/TodoRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoRepository extends MongoRepository<TodoDTO, String> {
-     
+
 }
