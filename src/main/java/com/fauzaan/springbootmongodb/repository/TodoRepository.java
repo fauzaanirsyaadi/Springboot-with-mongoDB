e18050eb5a5b5a69bd4fe2e78cb662846a57a593
fauzaanirsyaadi <fauzaan.irsyaadi@gmail.com>
2021-08-12 13:09:03 +0700

    [CREATE] add model and repository

---
+++ src/main/java/com/fauzaan/springbootmongodb/repository/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.fauzaan.springbootmongodb.repository;
+
+import com.fauzaan.springbootmongodb.model.TodoDTO;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends MongoRepository<TodoDTO, String> {
+     
+}
